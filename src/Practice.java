9bbd3ce92dc3e3a411e80eb49b15f42a0eddf14d
Kavya Sri <kavyapulipati13@gmail.com>
2021-09-24 17:23:32 +0530

    practiced if/else

--- src/Practice.java
+++ src/Practice.java
@@ -2,16 +2,28 @@ class Practice {
     public static void main(String[] args) {
         practiceIfElse();
         practiceIfElse2();
+        practiceIfElse3();
+    }
+
+    private static void practiceIfElse3() {
+        int numberOfGirls = 25;
+        int numberOfBoys = 30;
+        int totalStrength = 55;
+        if (totalStrength == numberOfGirls + numberOfBoys)
+            System.out.println("totalStrength equal to numberOfGirls + numberOfBoys");
+        else
+            System.out.println("totalStrength not equal to numberOfGirls + numberOfBoys");
+
     }
 
     private static void practiceIfElse2() {
-       int choclates = 15;
-       int iceCreams = 20;
-       int balls     = 50;
-       if (balls > choclates + iceCreams)
-           System.out.println("balls greater than choclates + iceCreams");
-       else
-           System.out.println("balls not greater than choclates + iceCreams");
+        int choclates = 15;
+        int iceCreams = 20;
+        int balls = 50;
+        if (balls > choclates + iceCreams)
+            System.out.println("balls greater than choclates + iceCreams");
+        else
+            System.out.println("balls not greater than choclates + iceCreams");
     }
 
     private static void practiceIfElse() {
