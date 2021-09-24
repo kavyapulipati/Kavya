0a463691b1248cee384f8b6497896811beb7768f
Kavya Sri <kavyapulipati13@gmail.com>
2021-09-24 16:41:28 +0530

    practiced if/else

---
+++ src/Practice.java
@@ -0,0 +1,20 @@
+class Practice {
+    public static void main(String[] args) {
+        practiceIfElse();
+        practiceIfElse2();
+    }
+
+    private static void practiceIfElse2() {
+
+    }
+
+    private static void practiceIfElse() {
+        int a = 50;
+        int b = 5;
+        int c = 100;
+        if (c < (a * b))
+            System.out.println("c less than a*b");
+        else
+            System.out.println("c not less than a*b");
+    }
+}
